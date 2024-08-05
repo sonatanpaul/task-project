ba69535594cf428f9a588caaf4aec5266cf38dc7
sonatanpaul <iamsonatanpaul@gmail.com>
2024-08-05 10:43:52 +0600

    some error fixed

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,12 @@
-import Logo from "../assets/lws-logo-en.svg";
 export default function Header() {
   return (
     <>
       <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
         <div className="container mx-auto flex items-center justify-between gap-x-6">
           <a href="/">
-            <img className="h-[45px]" src={Logo} alt="Lws" />
+            <p className="text-3xl font-medium">
+              <span className="text-[#F5BF42]">Paul</span> Tasker
+            </p>
           </a>
         </div>
       </nav>
--- src/component/task/TaskActions.jsx
+++ src/component/task/TaskActions.jsx
@@ -1,5 +1,10 @@
 // eslint-disable-next-line react/prop-types
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function TaskActions({ onAddClick, onDeleteAllTask }) {
+  const notify = () => toast("Delete All Task Successful!");
+
   return (
     <>
       <div className="mb-14 items-center justify-between sm:flex">
@@ -11,12 +16,15 @@ export default function TaskActions({ onAddClick, onDeleteAllTask }) {
           >
             Add Task
           </button>
-          <button
-            onClick={onDeleteAllTask}
-            className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold"
-          >
-            Delete All
-          </button>
+          <div>
+            <button
+              onClick={onDeleteAllTask}
+              className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold"
+            >
+              Delete All
+            </button>
+            <ToastContainer />
+          </div>
         </div>
       </div>
     </>
